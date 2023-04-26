cef2e1d7407b95c408f1e756659a1809bc4228e2
Rahul P S <rahulps995@gmail.com>
2023-04-27 00:01:01 +0530

    delete build

--- public/src/utils/axios.js
+++ public/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  // baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000/api",
   baseURL: "https://gamersden.tech/api",
   //baseURL: "https://gamersden.tech/",
 
--- public/src/utils/baseurl.js
+++ public/src/utils/baseurl.js
@@ -1,3 +1,5 @@
 const BASE_URL = "https://gamersden.tech/api";
-//const BASE_URL = "http://localhost:3000";
+//const BASE_URL = "http://localhost:3000/api";
+//sdsnj//
+
 export default BASE_URL;
